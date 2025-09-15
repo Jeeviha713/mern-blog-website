4098e5868d0d93dbcb7c5333c2455b7999b5ffd7
Jeeviha713 <jeevihas11@gmail.com>
2025-09-15 20:43:42 +0530

    Update Post.jsx

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 
 export default function post({post}) {
-   const PF = "http://localhost:5000/images/";
+   const PF = "https://blog-backend-vgbv.onrender.com";
 return(
     <div className="post">
 {post.photo && (
